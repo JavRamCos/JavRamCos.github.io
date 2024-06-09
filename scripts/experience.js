ae2c4f955c66d33c9d46ce67862155e58c5d8152
Javier Ramirez Cospin <ram18099@uvg.edu.gt>
2024-06-08 20:09:32 -0600

    Updated experience.html
    - Added Work Experience div in experience.html
    Added UVG Specialist 2 Work & details to Work Experience div in experience.html
    - Added popWorks Function in experience.js to populate Work Experiences with data from lang.js
    - Updated experience.css to include div dimensions & Work Experience styling

--- scripts/experience.js
+++ scripts/experience.js
@@ -4,7 +4,7 @@ import {data} from './Utils/langs.js';
 
 const popInternships = (lang) => {
     document.getElementById('internships-title').innerText = data[lang].experience.internship_title;
-    const internshipsDivs = document.querySelectorAll(".experience-div");
+    const internshipsDivs = document.querySelectorAll(".internship");
     internshipsDivs.forEach((el, i) => {
         let exp = `internship_${i}`;
         document.getElementById(`${exp}_title`).innerText = data[lang].experience[exp].title;
@@ -20,6 +20,23 @@ const popInternships = (lang) => {
     });
 };
 
+const popWorks = (lang) => {
+    document.getElementById('works-title').innerText = data[lang].experience.work_title;
+    const worksDivs = document.querySelectorAll('.work');
+    worksDivs.forEach((el, i) => {
+        let exp = `work_${i}`;
+        document.getElementById(`${exp}_title`).innerText = data[lang].experience[exp].title;
+        document.getElementById(`${exp}_company`).innerText = data[lang].experience[exp].company;
+        document.getElementById(`${exp}_date`).innerText = data[lang].experience[exp].date;
+        const tasksDivs = el.querySelector('.experience-body-description')
+            .querySelectorAll('p');
+        tasksDivs.forEach((elem, x) => {
+            let task = `task_${x}`;
+            document.getElementById(`${exp}_${task}`).innerText = data[lang].experience[exp][task];
+        });
+    })
+}
+
 const updateLanguage = () => {
     const langSelected = getCookie('language-selected');
     // MAIN TITLE
@@ -27,6 +44,7 @@ const updateLanguage = () => {
     // INTERNSHIPS
     popInternships(langSelected);
     // WORK EXPERIENCES
+    popWorks(langSelected);
     // FREELANCE WORKS
     // CONTACT
     document.getElementById('contact-title').innerText = data[langSelected].contact.title;
